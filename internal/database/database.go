f5fb3acc5c6598e7821fb98c2153ab50e32ba11e
Jeffrey <jeffreyjianyujie@gmail.com>
2023-01-24 02:28:58 +0800

    Connected clearDB

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,7 +19,21 @@ func GetDB() (*sql.DB, error) {
 
 	var db *sql.DB
 	fmt.Println("Connecting to DB")
-	db, err := sql.Open("mysql", "root:gya1ydxf@tcp(127.0.0.1:3306)/forum")
+	// for localhost use
+	// db, err := sql.Open("mysql", "root:gya1ydxf@tcp(127.0.0.1:3306)/forum")
+
+	// hardcoded address for clearDB connection
+	// db, err := sql.Open("mysql", "b83f3be08aeca3:acf4cd99@tcp(us-cdbr-east-06.cleardb.net)/heroku_375321b59849bf2")
+
+	cfg := mysql.Config{
+		User:                 "b83f3be08aeca3",
+		Passwd:               "acf4cd99",
+		Net:                  "tcp",
+		Addr:                 "us-cdbr-east-06.cleardb.net",
+		DBName:               "heroku_375321b59849bf2",
+		AllowNativePasswords: true,
+	}
+	db, err := sql.Open("mysql", cfg.FormatDSN())
 
 	if err != nil {
 		fmt.Println("Error connecting to DB:", err)
